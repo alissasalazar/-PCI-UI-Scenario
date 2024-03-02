e393011e35bd6196390184c60373bdb990178413
Salazar.A <Salazar.A@RSAP-LPT-16.royalsystems.net>
2024-03-02 14:41:41 -0500

    Task #7

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,20 +1,34 @@
+import React, { useMemo, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { ColDef, GridApi } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import { useMemo, useRef } from "react";
-import style from "./styles/style.module.css";
+import style from "./styles/style.module.css"; 
 
+// Helper Functions
+const formatDate = (date: any) => {
+  const formatDate = new Date(date.value);
+  return formatDate.toISOString().split("T")[0];
+};
+
+const formatPha = (string: any) => {
+  if (string.value === "Y") {
+    return "Yes";
+  } else if (string.value === "N") {
+    return "No";
+  } else {
+    return "";
+  }
+};
+
+// Constants
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation" },
   {
     field: "discovery_date",
     headerName: "Discovery Date",
-    valueFormatter: (date) => {
-      const formatDate = new Date(date.value);
-      return formatDate.toISOString().split("T")[0];
-    },
+    valueFormatter: formatDate,
   },
   { field: "h_mag", headerName: "H (mag)" },
   { field: "moid_au", headerName: "MOID (au)" },
@@ -25,22 +39,17 @@ const columnDefs: ColDef[] = [
   {
     field: "pha",
     headerName: "Potentially Hazardous",
-    valueFormatter: (string) => {
-      if (string.value === "Y") {
-        return "Yes";
-      } else if (string.value === "N") {
-        return "No";
-      } else {
-        return "";
-      }
-    },
+    valueFormatter: formatPha,
   },
   { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
+  
+  // Refs
   const gridApiRef = useRef<GridApi | null>(null);
 
+  // Memoized default column definition
   const defautColDef = useMemo(
     () => ({
       sortable: true,
@@ -48,6 +57,8 @@ const NeoGrid = (): JSX.Element => {
     }),
     []
   );
+
+  // Clear Filters and Sorters Function
   const clearFiltersAndSort = () => {
     const gridApi = gridApiRef.current;
     if (gridApi) {
@@ -55,13 +66,13 @@ const NeoGrid = (): JSX.Element => {
       gridApi.setFilterModel(null);
     }
   };
+
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
       <h1>Near-Earth Object Overview</h1>
-      <button onClick={clearFiltersAndSort} className={style.clearButton }>
-      Clear Filters and Sorters
-      </button>
-
+      <button onClick={clearFiltersAndSort} className={style.clearButton}>
+        Clear Filters and Sorters
+      </button>{" "}
       <AgGridReact
         rowData={data}
         columnDefs={columnDefs}
